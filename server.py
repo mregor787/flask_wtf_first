aabedbfac35be761d2620739d0853f9a20d6b1d2
Egor Belousov <egor0288354@yandex.ru>
2021-04-06 20:33:19 +0300

    Modify server.py (task #2)

--- server.py
+++ server.py
@@ -9,5 +9,10 @@ def index(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
