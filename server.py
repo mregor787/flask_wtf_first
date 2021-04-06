90db40417055c0a57c1d282d34f6da82103b57ee
Egor Belousov <egor0288354@yandex.ru>
2021-04-06 21:08:38 +0300

    Task #3 (Profession list)

--- server.py
+++ server.py
@@ -14,5 +14,17 @@ def training(prof):
     return render_template('training.html', prof=prof)
 
 
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    prof_list = [
+        'Инженер-исследователь', 'Пилот', 'Строитель', 'Экзобиолог',
+        'Врач', 'Инженер по терраформированию', 'Климатолог',
+        'Специалист по радиационной защите', 'Астрогеолог', 'Гляциолог',
+        'Инженер жизнеобеспечения', 'Метеоролог', 'Оператор марсохода',
+        'Киберинженер', 'Штурман', 'Пилот дронов'
+    ]
+    return render_template('list_prof.html', list=list, prof_list=prof_list)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
